1b9929b4a6620da549af2bff4aee26e4740ba963
radu-bors <github.artuproar@8alias.com>
2023-08-01 23:55:39 +0200

    Improved code readability

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 This scripts simulates the electrical circuit defined in Assignment.pdf for 10s. It prints out readings from the voltmeter V and ammeter A at regular intervals (100 ms and 300 ms respectively) together with the instantaneous resistance RL every 1s. Additionally, every 1s it outputs the 2s rolling average RL.
 """
 
-import asyncio # required to run tasks in parallel
-import time # required for timestamps
+import asyncio
 
 from classes import Application
 
